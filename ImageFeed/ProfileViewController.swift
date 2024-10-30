078197caa26da4abe137e03ce6f07fae12eba1fa
amonraset <minia777@icloud.com>
2024-10-30 21:48:45 +0300

    s1

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by sm on 30.10.2024.
+//
+import UIKit
+
+class ProfileViewController {
+    
+    @IBOutlet weak var PhotoProfile: UIImageView!
+    
+    @IBOutlet weak var UserName: UILabel!
+    
+    @IBOutlet weak var UserEmail: UILabel!
+    
+    @IBOutlet weak var Usertext: UILabel!
+    
+    
+    @IBAction func Exit(_ sender: Any) {
+    }
+}
+
