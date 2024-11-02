148d89352b15e64ed785c75722a1872a7e526b98
amonraset <minia777@icloud.com>
2024-11-03 00:07:02 +0300

    7/7

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -8,9 +8,19 @@ import UIKit
 
 class SingleImageViewController: UIViewController {
     
-    var image: UIImage?
+    var image: UIImage?{
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
