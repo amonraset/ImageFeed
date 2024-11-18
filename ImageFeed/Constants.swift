894dc169bd572b94849aa7c0a7c59b5126435a9d
amonraset <minia777@icloud.com>
2024-11-19 00:17:20 +0300

    Web view done

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 enum Constants {
-    
-    static let accessKey = "a9f2wjNRYbDDnHSPz26AOp-fxy0tqSaUYxIy3371R_c"
-    static let secretKey = "sWGU0H5FvDUY35cQD-7guNc7GLmiVhnY4DbFihSL900"
-    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "Public access + Read user access + Write user access"
-    static let defaultBaseURL = URL(string:"https://api.unsplash.com")!
-    
+    static let accessKey = "<ваш Access Key>"
+    static let secretKey = "<ваш Secret Key>"
+    static let redirectURI = "<ваш Redirect URI>"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
 }
 
